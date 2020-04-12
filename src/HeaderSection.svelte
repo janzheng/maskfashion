b9735a1addfc46d38fdb9c41409c4186ba4dd97d
Haley Barlar <haley.barlar@blacklane.com>
2020-04-11 21:46:16 -0400

    navbar and hero sections styled, need to make hamburger menu and access p tag in markup in hero

---
+++ src/HeaderSection.svelte
@@ -0,0 +1,93 @@
+<section class="Header-section">
+	<a class="Logotype" href="#home">MaskFashion</a>
+	<div class="Menu">
+		<div class="desktop-links">
+			<a href="#buy">Buy a Mask</a>
+			<a href="#make">Make a Mask</a>
+			<a href="#donate">Donate a Mask</a>
+		</div>
+		<div class="mobile-burger">
+			<button
+				aria-haspopup="true"
+				aria-label="Navigation menu"
+			>
+				<svg 
+					xmlns="http://www.w3.org/2000/svg" 
+					height="24" 
+					viewBox="0 0 24 24" 
+					width="24" 
+					aria-hidden="true"
+				>
+    				<path d="M0 0h24v24H0z" fill="none"/>
+    				<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+				</svg>
+				<span class="sr-only">Click to open navbar</span>
+			</button>
+		</div>
+	</div>
+</section>
+
+
+<style type="text/scss">
+  @import './styles/core';
+
+
+//    general app styles 
+  .Header-section {
+  	display: flex;
+    justify-content: space-between;
+	height: 70px;
+	padding: 20px 0;
+
+	a {
+		text-decoration: none;
+		color: black;
+	}
+
+	.Logotype {
+		font-size: 24px;
+	}
+
+    .Menu {
+		text-align: right;
+		
+    	a {
+			padding-left: $unit;
+		}
+		
+		.desktop-links {
+			display: none;
+		}
+
+		.mobile-burger {
+			.sr-only {
+				height: 1px;
+				overflow: hidden;
+				position: absolute;
+				width: 1px;
+			}
+		}
+    }
+  }
+
+  @media (min-width: 768px) {
+	  .Header-section{
+		  .Menu {
+			  .desktop-links {
+				  display: inline;
+				  font-size: 18px;
+			  }
+
+			  .mobile-burger {
+				  display: none;
+			  }
+		  }
+	  }
+
+  }
+
+
+
+</style>
+
+
--- src/sections/HeroSection.svelte
+++ src/sections/HeroSection.svelte
@@ -1,10 +1,17 @@
 
 <div class="Hero-section">
-	<div class="Hero">
-		<h2>“My mask protects you, your mask protects me.”</h2>
-		<h3>#maskfashion: discover mask makers</h3>
+	<div class="image-tagline">
+		<img
+			src="images/hero.jpg"
+			alt="Illustrated group of people wearing face masks"
+		/>
+
+		<div class="hero-tagline">
+			<h2>“My mask protects you, your mask protects me.”</h2>
+			<h3>#maskfashion: discover mask makers</h3>
+		</div>
 	</div>
-	<div>
+	<div class="explainer">
 		{@html marked(explainer)}
 		<!-- At Mask Fashion, we are trying to normalize mask wearing in order to create a safer place for everyone. With the Coronavirus pandemic, the CDC recommends everyone wear a mask in order to mitigate the risk for spreading the virus. So, let's get creative with our masks! Let's encourage displaying and wearing awesome, great-looking masks in public. I used to feel weird wearing my mask outside, but now with more people doing it, I feel better about wearing mine in public. -->
 	</div>
@@ -33,8 +40,78 @@
 </script>
 
 <style type="text/scss">
-  @import '../styles/core';
+	@import './styles/core';
 
+	.Hero-section {
+		.image-tagline {
+			margin-left: -32px;
+			margin-right: -32px;
+			position: relative;
+			height: 368px;
+
+			img {
+				width: 100%;
+				height: 100%;
+				object-fit: cover;
+			}
+
+			.hero-tagline {
+				position: absolute;
+				top: 115px;
+				left: 30px;
+				width: 225px;
+				background: white;
+				border-radius: 10px;
+				padding: 14px 12px 26px 12px;
+				font-weight: 300;
+
+				h2, h3 {
+					font-weight: 400;
+				}
+
+				h2 {
+					font-size: 30px;
+					line-height: 35px;
+					padding-bottom: 12px;
+				}
+
+				h3 {
+					font-size: 16px;
+					line-height: 19px;
+					padding-bottom: 0;
+				}
+			}
+		}
+
+		.explainer {
+			padding: 20px 0;
+		}
+	}
+
+	@media (min-width: 768px) {
+		.Hero-section {
+			.image-tagline {
+				height: 500px;
+
+				.hero-tagline {
+					width: 413px;
+					padding: 45px 48px;
+					top: 80px;
+    				left: 45px;
+					
+					h2 {
+						font-size: 38px;
+    					line-height: 47px;
+					}
+
+					h3 {
+						font-size: 28px;
+    					line-height: 35px;
+					}
+				}
+			}
+		}
+	}
 </style>
 
 
